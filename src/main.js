52c8c4bf97ccf6781c2def604d687c371ae4507d
Abdullah Asghar <203540047+abdullahasghar966@users.noreply.github.com>
2026-08-10 21:58:20 +0500

    Revert "Add THE WALK: an infinite hand-drawn corridor"
    This reverts commit 6132132a89955799addf3f5d3999abf6bd863bc5.

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ import { initMarquees } from './js/marquee.js';
 import { initHorizontal } from './js/horizontal.js';
 import { initClock } from './js/clock.js';
 import { initTelemetry } from './js/telemetry.js';
-import { initWalk } from './js/walk.js';
 import { setInitialStates, heroIntro, initReveals } from './js/reveals.js';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -34,7 +33,6 @@ async function init() {
   initClock();
   initMarquees();
   initReveals();
-  initWalk();
   initHorizontal();
   initTelemetry();
 
